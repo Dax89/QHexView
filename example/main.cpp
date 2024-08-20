598a808cddde6297b9ec975c931c1e592d796b90
Johannes Wilde <git@jowil.de>
2024-08-20 22:53:10 +0200

    Simplify example [mainly] as proposed by Dax89.

--- example/main.cpp
+++ example/main.cpp
@@ -1,25 +1,14 @@
-#include "qhexview.h"
-
 #include <QApplication>
-#include <QByteArray>
-#include <QFile>
+#include <qhexview.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
-    QApplication a(argc, argv);
+    QApplication a{argc, argv};
 
-    // Get data:
-    QFile file(QApplication::applicationFilePath());
-    QByteArray bytes;
-    if (file.exists()) {
-        file.open(QFile::ReadOnly);
-        bytes = file.readAll();
-        file.close();
-    }
+    QHexDocument* const doc = QHexDocument::fromFile(QApplication::applicationFilePath());
 
-    // Create hexview
     QHexView view;
-    view.setData(bytes);
+    view.setDocument(doc); // No parent, it takes the ownership of 'doc'
     view.show();
 
     return a.exec();
